0a95884a7feb7b1cc2b17dd61ef640b2bcd62925
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-19 16:18:00 -0400

    set up of scoring boxes and trackers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   
   var winscount = 0;
-
-    $("#winslocation").append("Wins: " + winscount);
-
-    var lossescount = 0;
-
-
+    $("#winslocation").html("Wins: <h4>" + winscount + "</h4>");
+  var lossescount = 0;
+    $("#losseslocation").html("Losses: <h4>" + lossescount + "</h4>");
+  var startingscore = 0;
+    $("#currentscorebox").html("Your total score is: <h2>" + startingscore + "</h2>")
+  var randomgoalnumber = 0;
+    $("#goalnumlocation").html("Your goals is to match: <h2>" + randomgoalnumber + "</h2>")
 
 
     

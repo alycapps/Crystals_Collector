c0d0cd05d5ad754993504e147c70469aee664cff
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-20 16:10:28 -0400

    random numbers for gems added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,11 +4,30 @@ $(document).ready(function() {
     $("#winslocation").html("Wins: <h4>" + winscount + "</h4>");
   var lossescount = 0;
     $("#losseslocation").html("Losses: <h4>" + lossescount + "</h4>");
-  var startingscore = 0;
-    $("#currentscorebox").html("Your total score is: <h2>" + startingscore + "</h2>")
-  var randomgoalnumber = 0;
+  var currentscore = 0;
+    $("#currentscorebox").html("Your total score is: <h2>" + currentscore + "</h2>")
+  var randomgoalnumber = Math.floor((Math.random() *100) + 20);
     $("#goalnumlocation").html("Your goals is to match: <h2>" + randomgoalnumber + "</h2>")
+
+  var gem1value = Math.floor((Math.random() *11) + 1);
+    console.log(gem1value);
+
+  var gem2value = Math.floor((Math.random() *11) + 1);
+  console.log(gem2value);
+
+  var gem3value = Math.floor((Math.random() *11) + 1);
+  console.log(gem3value);
+
+  var gem4value = Math.floor((Math.random() *11) + 1);
+  console.log(gem4value);
+
+  $(".gem").on("click", function () {
+    currentscore ++;
+    console.log("current score" + currentscore);
+    $("#currentscorebox").html("Your total score is: <h2>" + currentscore + "</h2>")
+  });
+
 
 
     
-  });
+});

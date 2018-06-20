6039c6a3683c5e68935e937391841789d01bab04
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-20 17:42:07 -0400

    game reset function added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,10 +24,31 @@ $(document).ready(function() {
   function winorlose() {
     if (currentscore === randomgoalnumber) {
       console.log("you win");
+      winscount ++;
+      $("#winslocation").html("Wins: <h4>" + winscount + "</h4>");
+      resetgame();
     }
     else if (currentscore > randomgoalnumber) {
       console.log("you lose");
+      lossescount ++;
+      $("#losseslocation").html("Losses: <h4>" + lossescount + "</h4>");
+      resetgame();
     }   
+  }
+
+  function resetgame() {
+    currentscore = 0;
+    $("#currentscorebox").html("Your total score is: <h2>" + currentscore + "</h2>")
+    randomgoalnumber = Math.floor((Math.random() *100) + 20);
+    $("#goalnumlocation").html("Your goals is to match: <h2>" + randomgoalnumber + "</h2>")
+    gem1value = Math.floor((Math.random() *11) + 1);
+    console.log(gem1value);
+    gem2value = Math.floor((Math.random() *11) + 1);
+    console.log(gem2value);
+    gem3value = Math.floor((Math.random() *11) + 1);
+    console.log(gem3value);
+    gem4value = Math.floor((Math.random() *11) + 1);
+    console.log(gem4value);
   }
 
   $("#gem1").on("click", function () {

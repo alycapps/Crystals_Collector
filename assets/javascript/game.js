2e0e8a330a2a4e132f27b05502cc52fc63954099
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-21 15:45:40 -0400

    cleaning up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,27 +10,21 @@ $(document).ready(function() {
     $("#goalnumlocation").html("Your goals is to match: <h2>" + randomgoalnumber + "</h2>")
 
   var gem1value = Math.floor((Math.random() *11) + 1);
-    console.log(gem1value);
 
   var gem2value = Math.floor((Math.random() *11) + 1);
-  console.log(gem2value);
 
   var gem3value = Math.floor((Math.random() *11) + 1);
-  console.log(gem3value);
 
   var gem4value = Math.floor((Math.random() *11) + 1);
-  console.log(gem4value);
 
   function winorlose() {
     if (currentscore === randomgoalnumber) {
-      console.log("you win");
       winscount ++;
       $("#winslocation").html("Wins: <h4>" + winscount + "</h4>");
       resetgame();
       $("#winorloselocation").html("<h5>" + "You win!" + "</h5>");
     }
     else if (currentscore > randomgoalnumber) {
-      console.log("you lose");
       lossescount ++;
       $("#losseslocation").html("Losses: <h4>" + lossescount + "</h4>");
       resetgame();
@@ -45,18 +39,13 @@ $(document).ready(function() {
     randomgoalnumber = Math.floor((Math.random() *100) + 20);
     $("#goalnumlocation").html("Your goals is to match: <h2>" + randomgoalnumber + "</h2>")
     gem1value = Math.floor((Math.random() *11) + 1);
-    console.log(gem1value);
     gem2value = Math.floor((Math.random() *11) + 1);
-    console.log(gem2value);
     gem3value = Math.floor((Math.random() *11) + 1);
-    console.log(gem3value);
     gem4value = Math.floor((Math.random() *11) + 1);
-    console.log(gem4value);
   }
   
 function gemclick(gem) {
   currentscore += gem;
-  console.log("current score" + currentscore);
   $("#currentscorebox").html("Your total score is: <h2>" + currentscore + "</h2>");
   winorlose();
 }

411dc79bc17b485da2646ef95712826ec99d458d
Aly <38168385+alycapps@users.noreply.github.com>
2018-06-23 09:40:06 -0400

    cleaning up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
   var randomgoalnumber = Math.floor((Math.random() *100) + 20);
     $("#goalnumlocation").html("Your goals is to match: <h2>" + randomgoalnumber + "</h2>")
 
-  // var gem1value = Math.floor((Math.random() *11) + 1);
 
   function gemvalue(id) {
     $(id).data("value", Math.floor((Math.random() *11) + 1));
@@ -18,11 +17,6 @@ $(document).ready(function() {
   for (i=1; i<5; i++) {
     gemvalue("#gem" + i);
   }
-  // var gem2value = Math.floor((Math.random() *11) + 1);
-
-  // var gem3value = Math.floor((Math.random() *11) + 1);
-
-  // var gem4value = Math.floor((Math.random() *11) + 1);
 
   function winorlose() {
     if (currentscore === randomgoalnumber) {
@@ -50,34 +44,11 @@ $(document).ready(function() {
     gem3value = Math.floor((Math.random() *11) + 1);
     gem4value = Math.floor((Math.random() *11) + 1);
   }
-  
-// function gemclick(gem) {
-//   currentscore += gem;
-//   $("#currentscorebox").html("Your total score is: <h2>" + currentscore + "</h2>");
-//   winorlose();
-// }
 
 $(".gem img").on("click", function () {
     currentscore += $(this).data("value");
     $("#currentscorebox").html("Your total score is: <h2>" + currentscore + "</h2>");
     winorlose();
   });
-
-
-  // $("#gem1").on("click", function () {
-  //   gemclick(gem1value);
-  // });
-
-  // $("#gem2").on("click", function () {
-  //   gemclick(gem2value);
-  // });
-
-  // $("#gem3").on("click", function () {
-  //   gemclick(gem3value);
-  // });
-
-  // $("#gem4").on("click", function () {
-  //   gemclick(gem4value);
-  // });
 
 });
